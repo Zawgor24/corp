e918f4ee25290f363b99d894ea3c6b6be00140db
igor <zawgorodskiy.2012@gmail.com>
2017-12-23 22:00:46 +0200

    sad

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,15 @@ if Rails.env.development?
                     password_confirmation: 'password')
 end
 
-lg = Corporation.create(name: 'LG', founded: DateTime.new.utc(1966, 2, 3),
+lg = Corporation.create(name: 'LG', founded: DateTime.new(1966, 2, 3),
                         founder: 'Koo In-hwoi',
                         revenue: 143, area_served: 'Worldwide', rating: 8)
 
-samsung = Corporation.create(name: 'Samsung', founded: DateTime.new.utc(1938, 3, 1),
+samsung = Corporation.create(name: 'Samsung', founded: DateTime.new(1938, 3, 1),
                              founder: 'Lee Byung-chul',
                              revenue: 305, area_served: 'Worldwide', rating: 9)
 
-apple = Corporation.create(name: 'Apple', founded: DateTime.new.utc(1976, 4, 1),
+apple = Corporation.create(name: 'Apple', founded: DateTime.new(1976, 4, 1),
                            founder: 'Steve Jobs',
                            revenue: 229.234, area_served: 'Worldwide', rating: 8)
 
